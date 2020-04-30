67539c04ce360cb36d35c8cc30444df42d593f0a
wangshuo1468 <51743731+wangshuo1468@users.noreply.github.com>
2020-04-30 17:09:24 +0800

    修改代码生成器路径，为当前项目风格

--- CicadasCms/src/main/java/com/zhiliao/mybatis/mapper/ConfigInfoAggrMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zhiliao.mybatis.mapper;
-
-import com.zhiliao.component.generator.mapper.MyMapper;
-import com.zhiliao.mybatis.model.ConfigInfoAggr;
-
-public interface ConfigInfoAggrMapper extends MyMapper<ConfigInfoAggr> {
-}
--- CicadasCms/src/main/java/com/zhiliao/mybatis/mapper/ConfigInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zhiliao.mybatis.mapper;
-
-import com.zhiliao.component.generator.mapper.MyMapper;
-import com.zhiliao.mybatis.model.ConfigInfo;
-
-public interface ConfigInfoMapper extends MyMapper<ConfigInfo> {
-}
